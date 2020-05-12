aefad6b9fd20678599779af359e1efa3f612e840
katelynbooth45 <katelynbooth45@gmail.com>
2020-05-11 21:55:24 -0400

    first commit

---
+++ frontend/src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { MatTableModule } from '@angular/material/table'; 
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ProductsComponent } from './products/products.component';
+import { MatButtonModule } from '@angular/material/button'; 
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ProductsComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatTableModule,
+    MatButtonModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
